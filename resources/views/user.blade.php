51f3f14ce89d01ed0bfb3d26964edbb4dfdd2d91
adhambaskara001 <adhambaskara2@gmail.com>
2025-03-06 00:13:19 +0700

    Prak2.6_J4

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -1,8 +1,10 @@
 <!DOCTYPE html>
 <html>
+
 <head>
     <title>Data User</title>
 </head>
+
 <body>
     <h1>Data User</h1>
     <table border="1" cellpadding="2" cellspacing="0">
@@ -11,13 +13,19 @@
             <th>Username</th>
             <th>Nama</th>
             <th>ID Level Pengguna</th>
+            <td>AAS1</td>
         </tr>
-        <tr>
-            <td>{{ $data->user_id }}</td>
-            <td>{{ $data->username }}</td>
-            <td>{{ $data->nama }}</td>
-            <td>{{ $data->level_id }}</td>
+        @foreach ($data as $d)
+            <tr>
+                <td>{{ $d->user_id }}</td>
+                <td>{{ $d->username }}</td>
+                <td>{{ $d->nama }}</td>
+                <td>{{ $d->level_id }}</td>
+                <td><a href="/user/ubah/{{ $d->user_id }}">ubah</a> | <a href="/user/hapus/{{ $d->user_id }}">hapus</a></td>
+            </tr>
+        @endforeach
         </tr>
     </table>
 </body>
+
 </html>
